b850fe6dbe7d386ebbc7522d2568a020beb2df6f
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-21 23:53:09 +0200

    modele DD3D

--- Projet_DL_JGorseBogoin.py
+++ Projet_DL_JGorseBogoin.py
@@ -4,16 +4,12 @@ import os
 import random
 import keras
 
-from keras import Input, Model
-from keras.models import Sequential
-from keras.layers import Dense, Flatten 
-from keras.layers import add, Activation
-from keras.layers import Conv1D, AveragePooling1D
-from keras.utils import np_utils
 from keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution3D, MaxPooling3D, Dropout, Flatten
+from keras.layers.advanced_activations import LeakyReLU
+from keras import backend as K
+
 
 
 def creation_control (nbre_individu):
@@ -76,6 +72,47 @@ def definition_de_y(echantillon_voxel, nucleotide, heme, control, steroid):
             Y.append(3)
     Y  = np.array(Y)
     return Y
+
+def creation_modele ():
+    model = Sequential()
+        # Conv layer 1
+        model.add(Convolution3D(
+            input_shape = (14,32,32,32),
+            filters=64,
+            kernel_size=5,
+            padding='valid',     # Padding method
+            data_format='channels_first',
+        ))
+        model.add(LeakyReLU(alpha = 0.1))
+        # Dropout 1
+        model.add(Dropout(0.2))
+        # Conv layer 2
+        model.add(Convolution3D(
+            filters=64,
+            kernel_size=3,
+            padding='valid',     # Padding method
+            data_format='channels_first',
+        ))
+        model.add(LeakyReLU(alpha = 0.1))
+        # Maxpooling 1
+        model.add(MaxPooling3D(
+            pool_size=(2,2,2),
+            strides=None,
+            padding='valid',    # Padding method
+            data_format='channels_first'
+        ))
+        # Dropout 2
+        model.add(Dropout(0.4))
+        # FC 1
+        model.add(Flatten())
+        model.add(Dense(128)) # TODO changed to 64 for the CAM
+        model.add(LeakyReLU(alpha = 0.1))
+        # Dropout 3
+        model.add(Dropout(0.4))
+        # Fully connected layer 2 to shape (2) for 2 classes
+        model.add(Dense(2))
+        model.add(Activation('softmax'))
+        return model   
 
 
 ###### MAIN ######
