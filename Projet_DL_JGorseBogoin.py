6e34bdfefe9d324b198fe8944220a3cf429c238c
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-21 23:47:29 +0200

    jeux de donnees ok

--- Projet_DL_JGorseBogoin.py
+++ Projet_DL_JGorseBogoin.py
@@ -10,9 +10,11 @@ from keras.layers import Dense, Flatten
 from keras.layers import add, Activation
 from keras.layers import Conv1D, AveragePooling1D
 from keras.utils import np_utils
+from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+
 
 def creation_control (nbre_individu):
     #Génération des listes contenant 50 poches choisies au hasard 
@@ -50,30 +52,31 @@ def generation_voxels (voxels_tot, steroid, heme, nucleotide, control):
     random.shuffle(echantillon_voxel)
     return echantillon_voxel
 
-def definition_de_x(path, chosen_pocket):
+def definition_de_x(path, echantillon_voxel):
     try:
-        X = [np.load("{}/{}".format(path, pocket))
-             for pocket in chosen_pocket]
+        X = [np.load("{}/{}".format(path, voxel))
+             for voxel in echantillon_voxel]
     except ValueError:
-        print("{}".format(pocket))
+        print("{}".format(voxel))
     X = [np.squeeze(array) for array in X]
     X = np.array(X)
     X = np.moveaxis(X, 1, -1)
     return X
 
-def definition_de_y(chosen_pocket, nucleotid, heme, control, steroid):
+def definition_de_y(echantillon_voxel, nucleotide, heme, control, steroid):
     Y = []
-    for pocket in chosen_pocket:
-        if pocket in nucleotid:
+    for voxel in echantillon_voxel:
+        if voxel.replace('.npy','') in nucleotide:
             Y.append(1)
-        elif pocket in heme:
+        elif voxel.replace('.npy','') in heme:
             Y.append(2)
-        elif pocket in steroid:
+        elif voxel.replace('.npy','') in steroid:
             Y.append(4)
-        elif pocket in control:
+        elif voxel.replace('.npy','') in control:
             Y.append(3)
     Y  = np.array(Y)
     return Y
+
 
 ###### MAIN ######
 
@@ -106,7 +109,6 @@ voxels_tot = list(filter(lambda fichier: fichier.endswith('.npy'), voxels_tot))
 
 
 ###### JEU D'APPRENTISSAGE ######
-
 train_control = creation_control(40)
 train_heme = creation_heme(40)
 train_steroid = creation_steroid(40)
@@ -115,11 +117,10 @@ train_voxels = generation_voxels(voxels_tot, train_steroid, train_heme, train_nu
 
 #Chargement des voxels
 X_train = definition_de_x(voxels_path, train_voxels)
-Y_train = definition_de_y(train_voxels, nucleotide_list, heme_list, control_list, steroid_list)
-
+Y_train = definition_de_y(train_voxels, train_nucleotide, train_heme, train_control, train_steroid)
+encoded_Y_train = to_categorical(Y_train)
 
 ###### JEU DE TEST ######
-
 test_control = creation_control(69)
 test_heme = creation_heme(69)
 test_steroid = creation_steroid(69)
@@ -127,8 +128,9 @@ test_nucleotide = creation_nucleotide(69)
 test_voxels = generation_voxels(voxels_tot, test_steroid, test_heme, test_nucleotide, test_control)
 
 #Chargement des voxels
-X_est = definition_de_x(voxels_path, test_voxels)
-Y_train = definition_de_y(test_voxels, nucleotide_list, heme_list, control_list, steroid_list)
+X_test = definition_de_x(voxels_path, test_voxels)
+Y_test = definition_de_y(test_voxels, test_nucleotide, test_heme, test_control, test_steroid)
+encoded_Y_test = to_categorical(Y_test)
 
 
 
