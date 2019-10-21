545819cf3b26299f16fbd3864a6292fac9d7831b
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-21 20:54:22 +0200

    génération des listes ok

--- Projet_DL_JGorseBogoin.py
+++ Projet_DL_JGorseBogoin.py
@@ -14,48 +14,34 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import os
 
-#Acces aux fichiers et generation des listes
-path = os.getcwd()
-oknotok = os.access(path + "/Data", mode=os.R_OK)
-if oknotok == FALSE:
-    print("Le repertoire Data n'existe pas. Le programme va s'arreter!")
-    break
-
-data_path = os.chroot(path + "/Data")
-print(data_path)
-
-control_file = "control.list.txt"
+#Génération des listes Poches
 control_list = []
-heme_file = "heme.list.txt"
 heme_list = []
-steroid_file = "steroid.list.txt"
 steroid_list = []
-nucleotid_file = "nucleotid.list.txt"
 nucleotid_list = []
 
-with open (control_file, "r") as fillin:
+with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/control_list.txt", "r") as fillin:
     for ligne in fillin:
         control_list.append(ligne)
 
-with open (heme_file, "r") as fillin:
+with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/heme_list.txt", "r") as fillin:
     for ligne in fillin:
         heme_list.append(ligne)
 
-with open (steroid_file, "r") as fillin:
+with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/steroid_list.txt", "r") as fillin:
     for ligne in fillin:
         steroid_list.append(ligne)
 
-with open (nucleotid_file, "r") as fillin:
+with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/nucleotide_list.txt", "r") as fillin:
     for ligne in fillin:
         nucleotid_list.append(ligne)
 
-oknotok = os.access(data_path + "/voxels", mode=os.R_OK)
-if oknotok == FALSE:
-    print("Le repertoire voxels n'existe pas. Le programme va s'arreter!")
-    break
+#Génération de la liste Voxels
+voxels_path = "/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/Voxels"
+voxels_tot = os.listdir(voxels_path)
+voxels_tot = list(filter(lambda fichier: fichier.endswith('.npy'), voxels_tot))
 
-voxel_path = os.chroot(data_path + "/voxels")
-voxel_list = os.listdir(voxel_path)
+
 
 
 
