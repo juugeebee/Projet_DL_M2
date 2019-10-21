59cba3b11aa5b83bceb9efac2b5018a4321b52f8
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-21 23:11:28 +0200

    jeu d'apprentissage

--- Projet_DL_JGorseBogoin.py
+++ Projet_DL_JGorseBogoin.py
@@ -1,10 +1,9 @@
 #Importation des modules
-import numpy as np
-from numpy import loadtxt
-import pandas 
-import keras
+import numpy as np 
 import os
 import random
+import keras
+
 from keras import Input, Model
 from keras.models import Sequential
 from keras.layers import Dense, Flatten 
@@ -15,60 +14,107 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 
+def creation_control (nbre_individu):
+    #Génération des listes contenant 50 poches choisies au hasard 
+    echantillon_control = random.sample(control_list, nbre_individu)
+    random.shuffle(echantillon_control)
+    return echantillon_control
+
+def creation_heme (nbre_individu):
+    echantillon_heme = random.sample(heme_list, nbre_individu)
+    random.shuffle(echantillon_heme)
+    return echantillon_heme
+
+def creation_steroid (nbre_individu):
+    echantillon_steroid = random.sample(steroid_list, nbre_individu)
+    random.shuffle(echantillon_steroid)
+    return echantillon_steroid
+
+def creation_nucleotide (nbre_individu): 
+    echantillon_nucleotide = random.sample(nucleotide_list, nbre_individu)  
+    random.shuffle(echantillon_nucleotide)
+    return echantillon_nucleotide
+
+def generation_voxels (voxels_tot, steroid, heme, nucleotide, control):
+    #Génération de la liste des voxels
+    echantillon_voxel = []
+    for voxel in voxels_tot:
+        if voxel.replace('.npy','') in steroid:
+            echantillon_voxel.append(voxel)
+        if voxel.replace('.npy','') in heme:
+            echantillon_voxel.append(voxel)
+        if voxel.replace('.npy','') in nucleotide:
+            echantillon_voxel.append(voxel)
+        if voxel.replace('.npy','') in control:
+            echantillon_voxel.append(voxel)
+    random.shuffle(echantillon_voxel)
+    return echantillon_voxel
+
+def definition_de_x(path, chosen_pocket):
+    try:
+        X = [np.load("{}/{}".format(path, pocket))
+             for pocket in chosen_pocket]
+    except ValueError:
+        print("{}".format(pocket))
+    X = [np.squeeze(array) for array in X]
+    X = np.array(X)
+    X = np.moveaxis(X, 1, -1)
+    return X
+
+def definition_de_y(chosen_pocket, nucleotid, heme, control, steroid):
+    Y = []
+    for pocket in chosen_pocket:
+        if pocket in nucleotid:
+            Y.append(1)
+        elif pocket in heme:
+            Y.append(2)
+        elif pocket in steroid:
+            Y.append(4)
+        elif pocket in control:
+            Y.append(3)
+    Y  = np.array(Y)
+    return Y
+
+###### MAIN ######
+
 #Génération des listes Poches
 control_list = []
 heme_list = []
 steroid_list = []
 nucleotide_list = []
 
-with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/control_list.txt", "r") as fillin:
+with open ("/Users/julieb/Data_DL/control_list.txt", "r") as fillin:
     for ligne in fillin:
         control_list.append(ligne.replace('\n',''))
         
-with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/heme_list.txt", "r") as fillin:
+with open ("/Users/julieb/Data_DL/heme_list.txt", "r") as fillin:
     for ligne in fillin:
         heme_list.append(ligne.replace('\n',''))
 
-with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/steroid_list.txt", "r") as fillin:
+with open ("/Users/julieb/Data_DL/steroid_list.txt", "r") as fillin:
     for ligne in fillin:
         steroid_list.append(ligne.replace('\n',''))
 
-with open ("/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/nucleotide_list.txt", "r") as fillin:
+with open ("/Users/julieb/Data_DL/nucleotide_list.txt", "r") as fillin:
     for ligne in fillin:
         nucleotide_list.append(ligne.replace('\n',''))
 
 #Génération de la liste Voxels
-voxels_path = "/Users/julieb/M2-BIB/DL_2.0/Projet_DL/Data/Voxels"
+voxels_path = "/Users/julieb/Data_DL/Voxels"
 voxels_tot = os.listdir(voxels_path)
 voxels_tot = list(filter(lambda fichier: fichier.endswith('.npy'), voxels_tot))
 
 ###### JEU D'APPRENTISSAGE ######
 
-#Génération des listes contenant 50 poches choisies au hasard 
-echantillon_control = random.sample(control_list, 50)
-random.shuffle(echantillon_control)
+train_control = creation_control(50)
+train_heme = creation_heme(50)
+train_steroid = creation_steroid(50)
+train_nucleotide = creation_nucleotide(50)
+train_voxels = generation_voxels(voxels_tot, train_steroid, train_heme, train_nucleotide, train_control)
 
-echantillon_heme = random.sample(heme_list, 50)
-random.shuffle(echantillon_heme)
-
-echantillon_steroid = random.sample(steroid_list, 50)
-random.shuffle(echantillon_steroid)
-
-echantillon_nucleotide = random.sample(nucleotide_list, 50)  
-random.shuffle(echantillon_nucleotide)
-
-#Génération de la liste des voxels
-echantillon_voxel = []
-for voxel in voxels_tot:
-    if voxel.replace('.npy','') in echantillon_steroid:
-        echantillon_voxel.append(voxel)
-    if voxel.replace('.npy','') in echantillon_heme:
-        echantillon_voxel.append(voxel)
-    if voxel.replace('.npy','') in echantillon_nucleotide:
-        echantillon_voxel.append(voxel)
-    if voxel.replace('.npy','') in echantillon_control:
-        echantillon_voxel.append(voxel)
-random.shuffle(echantillon_voxel)
+#Chargement des voxels
+X_train = definition_de_x(voxels_path, train_voxels)
+Y_train = definition_de_y(train_voxels, nucleotide_list, heme_list, control_list, steroid_list)
 
 
 
