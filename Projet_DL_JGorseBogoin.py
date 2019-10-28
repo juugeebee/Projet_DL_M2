6136783c757ce2874361cf8293692710b956b17c
juugeebee <july2023@yahoo.fr>
2019-10-28 14:33:56 +0100

    courbes de ROC

--- Projet_DL_JGorseBogoin.py
+++ Projet_DL_JGorseBogoin.py
@@ -1,3 +1,9 @@
+"""
+@author : Julie Bogoin
+Master 2 BIB - 2019 2020
+Projet Deep Learning
+"""
+
 #Importation des modules
 import numpy as np 
 import os
@@ -17,12 +23,13 @@ import sklearn.metrics as metrics
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
-
 from math import sqrt
 
+#Définition de la graine de random pour pouvoir reproduire les résultats
+np.random.seed(seed = 42)
 
 def creation_control (nbre_individu):
-    #Génération des listes contenant 50 poches choisies au hasard 
+    #Génération des listes contenant nbre_individu poches choisies au hasard 
     echantillon_control = random.sample(control_list, nbre_individu)
     random.shuffle(echantillon_control)
     return echantillon_control
@@ -42,12 +49,10 @@ def creation_nucleotide (nbre_individu):
     random.shuffle(echantillon_nucleotide)
     return echantillon_nucleotide
 
-def generation_voxels (voxels_tot, steroid, heme, nucleotide, control):
+def generation_voxels (voxels_tot, heme, nucleotide, control):
     #Génération de la liste des voxels
     echantillon_voxel = []
     for voxel in voxels_tot:
-        if voxel.replace('.npy','') in steroid:
-            echantillon_voxel.append(voxel)
         if voxel.replace('.npy','') in heme:
             echantillon_voxel.append(voxel)
         if voxel.replace('.npy','') in nucleotide:
@@ -72,13 +77,11 @@ def definition_de_y(echantillon_voxel, nucleotide, heme, control):
     Y = []
     for voxel in echantillon_voxel:
         if voxel.replace('.npy','') in nucleotide:
-            Y.append(0)
-        elif voxel.replace('.npy','') in heme:
             Y.append(1)
-        elif voxel.replace('.npy','') in control:
+        elif voxel.replace('.npy','') in heme:
             Y.append(2)
-        else:
-            Y.append(2)     
+        elif voxel.replace('.npy','') in control:
+            Y.append(3)    
     Y  = np.array(Y)
     return Y
 
@@ -161,83 +164,144 @@ voxels_tot = list(filter(lambda fichier: fichier.endswith('.npy'), voxels_tot))
 
 
 ###### JEU D'APPRENTISSAGE ######
-train_control = creation_control(40)
-train_heme = creation_heme(40)
-train_steroid = creation_steroid(40)
-train_nucleotide = creation_nucleotide(40)
-train_voxels = generation_voxels(voxels_tot, train_steroid, train_heme, train_nucleotide, train_control)
+train_control = creation_control(50)
+train_heme = creation_heme(50)
+train_nucleotide = creation_nucleotide(50)
+train_voxels = generation_voxels(voxels_tot, train_heme, train_nucleotide, train_control)
 
-#Chargement des voxels
 X_train = definition_de_x(voxels_path, train_voxels)
-print(X_train.shape)
+print("X_train : {}".format(X_train.shape))
 Y_train = definition_de_y(train_voxels, train_nucleotide, train_heme, train_control)
 encoded_Y_train = to_categorical(Y_train)
-print(encoded_Y_train.shape)
+print("Y_train : {}".format(encoded_Y_train.shape))
 
 ###### JEU DE TEST ######
-test_control = creation_control(69)
-test_heme = creation_heme(69)
-test_steroid = creation_steroid(69)
-test_nucleotide = creation_nucleotide(69)
-test_voxels = generation_voxels(voxels_tot, test_steroid, test_heme, test_nucleotide, test_control)
+test_control = creation_control(130)
+test_heme = creation_heme(130)
+test_nucleotide = creation_nucleotide(130)
+test_voxels = generation_voxels(voxels_tot, test_heme, test_nucleotide, test_control)
 
-#Chargement des voxels
 X_test = definition_de_x(voxels_path, test_voxels)
 Y_test = definition_de_y(test_voxels, test_nucleotide, test_heme, test_control)
 encoded_Y_test = to_categorical(Y_test)
 
-#Creation du modèle
+###### CREATION DU MODEL ######
 critor = EarlyStopping(monitor = "val_loss", patience = 3, mode = "min")
 my_model = creation_modele()
 my_model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=['accuracy'])
 
-my_model.fit(X_train, encoded_Y_train, epochs = 4, batch_size = 20,
+history = my_model.fit(X_train, encoded_Y_train, epochs = 1, batch_size = 20,
              validation_split = 0.1, callbacks = [critor])
+
+# #Accuracy
+# plt.plot(history.history['acc'])
+# plt.plot(history.history['val_acc'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+
+# #Loss
+# plt.plot(history.history['loss'])
+# plt.plot(history.history["val_loss"])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
+
+#Sauvegarder le model
+my_model.save('./my_model.h5')
 
 #Evaluation du modèle
 evaluation = my_model.evaluate(X_test, encoded_Y_test)
-print(evaluation)
+print("\nTest score: ", evaluation[0])
+print("Test accuracy: \n", evaluation[1])
 
-#training = KerasClassifier(build_faux_negatifs = my_model, epochs = 5, batch_size=20, verbose=2)
-#kfold = KFold(n_splits = 5, shuffle=True)
-# cv_result = cross_val_score(training, X_train, encoded_Y_train, cv = kfold)
-# print(cv_result)
-# print("%.2f%%(%2d%%)"%(cv_result.mean()*100, cv_result.std()*100))
+#Predictions
+predictions = my_model.predict(X_test)
 
+vrai_positifs = 0
+faux_positifs = 0
+vrai_negatifs = 0
+faux_negatifs = 0
 
-# predictions = my_model.predict(X_test)
-
-# vrai_positifs = 0
-# faux_positifs = 0
-# vrai_negatifs = 0
-# faux_negatifs = 0
-
-# for i in range(predictions.shape[0]):
-#     maxi = max(predictions[i,:])
-#     if maxi == predictions[i, 0]:
-#         classe = 0
-#     elif maxi == predictions[i,1]:
-#         classe = 1
-#     elif maxi == predictions[i,2]:
-#         classe = 2
+for i in range(predictions.shape[0]):
+    maxi = max(predictions[i,:])
+    if maxi == predictions[i, 0]:
+        classe = 0
+    elif maxi == predictions[i,1]:
+        classe = 1
+    elif maxi == predictions[i,2]:
+        classe = 2
         
-#     if (encoded_Y_test[i, 0] == 1.0) and (classe == 0):
-#         vrai_positifs += 1
-#     elif (encoded_Y_test[i, 1] == 1.0) and (classe == 1):
-#         vrai_positifs += 1
-#     elif (encoded_Y_test[i, 2] == 1.0) and (classe == 0):
-#         faux_positifs += 1
-#     elif (encoded_Y_test[i, 2] == 1.0) and (classe == 1):
-#         faux_positifs += 1
-#     elif (encoded_Y_test[i, 2] == 1.0) and (classe == 2):
-#         vrai_negatifs += 1
-#     elif (encoded_Y_test[i, 2] == 0.0) and (classe == 2):
-#         faux_negatifs += 1
+    if (encoded_Y_test[i, 0] == 1.0) and (classe == 0):
+        vrai_positifs += 1
+    elif (encoded_Y_test[i, 1] == 1.0) and (classe == 1):
+        vrai_positifs += 1
+    elif (encoded_Y_test[i, 2] == 1.0) and (classe == 0):
+        faux_positifs += 1
+    elif (encoded_Y_test[i, 2] == 1.0) and (classe == 1):
+        faux_positifs += 1
+    elif (encoded_Y_test[i, 2] == 1.0) and (classe == 2):
+        vrai_negatifs += 1
+    elif (encoded_Y_test[i, 2] == 0.0) and (classe == 2):
+        faux_negatifs += 1
 
-# print("vrai_positifs:{:.2f}%".format(vrai_positifs*100/len(predictions)))
-# print("faux_positifs:{:.2f}%".format(faux_positifs*100/len(predictions)))
-# print("vrai_negatifs:{:.2f}".format(vrai_negatifs*100/len(predictions)))
-# print("faux_negatifs:{:.2f}".format(faux_negatifs*100/len(predictions)))
+print("\nPourcentage de vrais positifs: {:.2f}%".format(vrai_positifs*100/len(predictions)))
+print("Pourcentage de faux positifs: {:.2f}%".format(faux_positifs*100/len(predictions)))
+print("Pourcentage de vrais negatifs: {:.2f}%".format(vrai_negatifs*100/len(predictions)))
+print("Pourcentage de faux negatifs: {:.2f}%\n".format(faux_negatifs*100/len(predictions)))
+
+### NUCLEOTIDES
+y_test_prediction_nucleotide = predictions[:,0]
+y_test_nucleotide = encoded_Y_test[:,0]
+nucleotide_fpr, nucleotide_tpr, nucleotide_thresholds = metrics.roc_curve(y_test_nucleotide, y_test_prediction_nucleotide, pos_label=2)
+nucleotide_roc_auc = metrics.auc(nucleotide_fpr, nucleotide_tpr)
+
+### HEME
+y_test_prediction_heme = predictions[:,1]
+y_test_heme = encoded_Y_test[:,1]
+heme_fpr, heme_tpr, heme_thresholds = metrics.roc_curve(y_test_heme, y_test_prediction_heme, pos_label=2)
+heme_roc_auc = metrics.auc(heme_fpr, heme_tpr)
+
+### CONTROL
+y_test_prediction_control = predictions[:,2]
+y_test_control = encoded_Y_test[:,2]
+control_fpr, control_tpr, control_thresholds = metrics.roc_curve(y_test_control, y_test_prediction_control, pos_label=2)
+control_roc_auc = metrics.auc(control_fpr, control_tpr)
+
+#Courbes de ROC: evaluate classifier output quality using cross-validation
+plt.title("Courbe ROC")
+plt.plot(control_fpr,
+        control_tpr,
+        color='darkblue',
+        linestyle='-',
+        linewidth=4,
+        label = "control AUC = %0.2f" % control_roc_auc)
+
+plt.plot(heme_fpr,
+        heme_tpr,
+        color='darkorange',
+        linestyle=':',
+        linewidth=4,
+        label = "heme AUC = %0.2f" % heme_roc_auc)
+
+plt.plot(nucleotide_fpr,
+        nucleotide_tpr,
+        color='deeppink',
+        linestyle=':',
+        linewidth=4,
+        label = "nucleotide AUC = %0.2f" % nucleotide_roc_auc)
+
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1.05])
+plt.ylabel("Sensibility")
+plt.xlabel("1-Sensibility")
+plt.show()
 
 
 
